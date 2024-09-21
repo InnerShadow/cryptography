35ead123aef17803a1e78a5ef78df25102bf9271
Nobody <mishasikolenko@mail.ru>
2024-09-21 22:32:25 +0300

    Fix PermutationЭ

--- Lab1/src/CryptoSystems/PermutationCipher.py
+++ Lab1/src/CryptoSystems/PermutationCipher.py
@@ -1,5 +1,7 @@
 from CryptoSystems.CryptographySystem import CryptographySystem
 
+MAX_TMP_FILL = 35
+
 class PermutationCipher(CryptographySystem):
     def _check_key(self) -> None:
         if len(set(self.key)) != len(self.key) or len(self.key) > self.M:
@@ -8,19 +10,25 @@ class PermutationCipher(CryptographySystem):
     # end def
 
     def _preprocess_key(self) -> None:
-        self.key_indices = [self.alphabet.index(k) for k in self.key]
+        key_indices = [self.alphabet.index(k) for k in self.key]
+        self.key_indices = [MAX_TMP_FILL] * len(self.key)
+        for i in range(len(self.key)):
+            self.key_indices[key_indices.index(min(key_indices))] = i
+            key_indices[key_indices.index(min(key_indices))] = MAX_TMP_FILL
+        # end for
     # end def
 
-    def _encrypt_single_character(self, symbol: str, position: int) -> str:
-        key_pos = position % len(self.key)
-        shift = self.key_indices[key_pos]
-        return self.alphabet[(self.alphabet.index(symbol) + shift) % self.M]
+    def _encrypt_single_character(self, symbol : str) -> str:
+        outout_str = ''
+        for i in self.key_indices:
+            outout_str += symbol[i]
+        # end for
+
+        return outout_str
     # end def
 
-    def _decrypt_single_character(self, symbol: str, position: int) -> str:
-        key_pos = position % len(self.key)
-        shift = self.key_indices[key_pos]
-        return self.alphabet[(self.alphabet.index(symbol) - shift) % self.M]
+    def _decrypt_single_character(self, symbol: str) -> str:
+        return self._encrypt_single_character(symbol)
     # end def
 
     def encrypt(self) -> str:
@@ -30,7 +38,12 @@ class PermutationCipher(CryptographySystem):
             self.input_str += self.alphabet[-1]
         # end while
 
-        res = ''.join(self.encrypt_function(char, i) for i, char in enumerate(self.input_str))
+        res = ''
+
+        for i in range(0, len(self.input_str), len(self.key)):
+            res += self.encrypt_function(self.input_str[i:i+len(self.key)])
+        # end for
+
         res = res if not num_padded_chars else res[:-num_padded_chars]
         
         self._write_output(res)
