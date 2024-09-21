447a0d2e4c9598cb67bca4a26a770e3644bc024a
Nobody <mishasikolenko@mail.ru>
2024-09-21 21:57:11 +0300

    Update CryptoWrapper

--- Lab1/src/CryptoSystems/CryptoWrapper.py
+++ Lab1/src/CryptoSystems/CryptoWrapper.py
@@ -15,48 +15,32 @@ class CryptoWrapper(object):
                  input_path : str = './Data/in.txt',
                  output_path : str = None) -> None:
         
+        common_args = {
+            'alphabet_path': alphabet_path,
+            'input_path': input_path,
+            'key_path': key_path,
+            'do_encrypt': do_encrypt,
+            'output_path': output_path
+        }
+        
         match method:
             case 'Caesar':
-                self.crypter = CaesarCipher(alphabet_path = alphabet_path,
-                                            input_path = input_path,
-                                            key_path = key_path,
-                                            do_encrypt = do_encrypt,
-                                            output_path = output_path)
+                self.crypter = CaesarCipher(**common_args)
             # end case
             case 'Affine':
-                self.crypter = AffineCipher(alphabet_path = alphabet_path,
-                                            input_path = input_path,
-                                            key_path = key_path,
-                                            do_encrypt = do_encrypt,
-                                            output_path = output_path)
+                self.crypter = AffineCipher(**common_args)
             # end case
             case 'Replace':
-                self.crypter = ReplaceCipher(alphabet_path = alphabet_path,
-                                            input_path = input_path,
-                                            key_path = key_path,
-                                            do_encrypt = do_encrypt,
-                                            output_path = output_path)
+                self.crypter = ReplaceCipher(**common_args)
             # end case
             case 'Hill':
-                self.crypter = HillCipher(alphabet_path = alphabet_path,
-                                            input_path = input_path,
-                                            key_path = key_path,
-                                            do_encrypt = do_encrypt,
-                                            output_path = output_path)
+                self.crypter = HillCipher(**common_args)
             # end case
             case 'Permutation':
-                self.crypter = PermutationCipher(alphabet_path = alphabet_path,
-                                            input_path = input_path,
-                                            key_path = key_path,
-                                            do_encrypt = do_encrypt,
-                                            output_path = output_path)
+                self.crypter = PermutationCipher(**common_args)
             # end case
             case 'Vigenere':
-                self.crypter = VigenereCipher(alphabet_path = alphabet_path,
-                                            input_path = input_path,
-                                            key_path = key_path,
-                                            do_encrypt = do_encrypt,
-                                            output_path = output_path)
+                self.crypter = VigenereCipher(**common_args)
             # end case
         # end math
     # end def 
